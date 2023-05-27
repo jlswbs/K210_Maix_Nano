00f4cb28e21f6f3344eb57b53dc4ac4adebfe9e2
jlswbs <jlswbs@users.noreply.github.com>
2023-05-27 19:32:10 +0200

    Add files via upload

---
+++ Pwm_Hopf_Osc/Pwm_Hopf_Osc.ino
@@ -0,0 +1,58 @@
+// Stereo Audio PWM - Hopf oscillator example //
+
+#include <stdio.h>
+#include <syslog.h>
+#include <timer.h>
+#include <pwm.h>
+#include <plic.h>
+#include <sysctl.h>
+#include <fpioa.h>
+
+#define SAMPLE_RATE 44100
+#define TIMER_RATE  1000000000/SAMPLE_RATE
+#define PWM_RATE    10*SAMPLE_RATE
+#define AUDIO_PIN_L 6
+#define AUDIO_PIN_R 8
+
+  float x = 0.01f;
+  float y = 0.01f;
+  float dt = 0.01f;
+  float p = 0.11f;
+
+int audio_callback(void *ctx){
+
+  float nx = x;
+  float ny = y;
+        
+  x = nx + dt * (-ny + nx * (p - (powf(nx, 2.0f) + powf(ny, 2.0f))));
+  y = ny + dt * (nx + ny * (p - (powf(nx, 2.0f) + powf(ny, 2.0f))));
+     
+  double duty_l = 0.48f + (1.3f * x);
+  double duty_r = 0.48f + (1.3f * y);
+
+  pwm_set_frequency(PWM_DEVICE_1, PWM_CHANNEL_0, PWM_RATE, duty_l);
+  pwm_set_frequency(PWM_DEVICE_1, PWM_CHANNEL_1, PWM_RATE, duty_r);
+
+  return 0;
+  
+}
+
+void setup(){
+
+  fpioa_set_function(AUDIO_PIN_L, FUNC_TIMER1_TOGGLE1);
+  fpioa_set_function(AUDIO_PIN_R, FUNC_TIMER1_TOGGLE2);
+  plic_init();
+  sysctl_enable_irq();
+  timer_init(TIMER_DEVICE_0);
+  timer_set_interval(TIMER_DEVICE_0, TIMER_CHANNEL_0, TIMER_RATE);
+  timer_irq_register(TIMER_DEVICE_0, TIMER_CHANNEL_0, 0, 1, audio_callback, NULL);
+  timer_set_enable(TIMER_DEVICE_0, TIMER_CHANNEL_0, 1);
+  pwm_init(PWM_DEVICE_1);
+  pwm_set_enable(PWM_DEVICE_1, PWM_CHANNEL_0, 1);
+  pwm_set_enable(PWM_DEVICE_1, PWM_CHANNEL_1, 1);
+    
+}
+
+void loop(){
+  
+}
